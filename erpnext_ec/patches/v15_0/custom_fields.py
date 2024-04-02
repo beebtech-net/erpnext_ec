d87847418f915b6b649a3e8645d65ff369a2afe4
Ronald Chonillo <ronaldchonillobt@beebtech.net>
2024-04-01 23:04:31 -0500

    [fix] old fields working

--- erpnext_ec/patches/v15_0/custom_fields.py
+++ erpnext_ec/patches/v15_0/custom_fields.py
@@ -2,8 +2,21 @@ from __future__ import unicode_literals
 import frappe
 import os
 
-from frappe.model.meta import rename_custom_field
-from frappe.model.meta import delete_custom_fields
+from frappe.model.meta import get_meta
+
+def rename_old_columns():
+    #And backup
+    print('before_execute - Rename old Custom Fields')
+
+    custom_fields_found = frappe.get_all("Custom Field", filters={"fieldname": "formapago", "dt": "Mode Of Payment"})
+    if custom_fields_found:
+        print('Campo encontrado')
+        print(custom_fields_found)
+        
+        #rename_custom_field("Mode Of Payment", "formapago", "formapago_remove")
+        meta = get_meta("Mode Of Payment")
+        meta.get_field("formapago").fieldname = "formapago_remove"
+        meta.save()
 
 def remove_old_columns():
     #And backup
@@ -11,11 +24,12 @@ def remove_old_columns():
 
     if frappe.get_all("Custom Field", filters={"fieldname": "formapago", "dt": "Mode Of Payment"}):
         print('Secuencial para el SRI')
-        rename_custom_field("Mode Of Payment", "formapago", "formapago_remove")
+        #delete_custom_fields("Mode Of Payment", "formapago", "formapago_remove")
+
 
 def add_new_columns():
     # Actualizacion de datos en los campos custom de módulos
     # estos datos serán extraidos desde los campos custom anteriores (en caso de ser encontrados)
     # estos campos previamente han sido renombrados y serán eliminados al terminar la migración de datos
     print('after_install - Remove old Custom Fields')
-    delete_custom_fields("Mode Of Payment", ["formapago"])
+    #delete_custom_fields("Mode Of Payment", ["formapago"])
