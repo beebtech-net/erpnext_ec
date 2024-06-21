dde878d3d5d205cc2ec667f1789be7e4c97182ca
Ronald Chonillo <ronald.chonillo@beebtech.net>
2024-06-21 16:34:11 -0500

    [fix] test-new-mode-10

--- erpnext_ec/hooks.py
+++ erpnext_ec/hooks.py
@@ -126,9 +126,9 @@ frappe_version_int = int(frappe_version.split('.')[0])
 jenv = {
      "methods": [
          "build_doc_fac:erpnext_ec.utilities.doc_builder_fac.build_doc_fac",
-#         "build_doc_fac_with_images:erpnext_ec.utilities.doc_builder_fac.build_doc_fac_with_images",
-#         "build_doc_cre:erpnext_ec.utilities.doc_builder_cre.build_doc_cre"
-#         "build_doc_cre_with_images:erpnext_ec.utilities.doc_builder_cre.build_doc_cre_with_images"
+         "build_doc_fac_with_images:erpnext_ec.utilities.doc_builder_fac.build_doc_fac_with_images",
+         "build_doc_cre:erpnext_ec.utilities.doc_builder_cre.build_doc_cre"
+         "build_doc_cre_with_images:erpnext_ec.utilities.doc_builder_cre.build_doc_cre_with_images"
      ],
      "filters": [
      ]
