82cbde3d7548c1e971a8dfec69f4613015da6b94
Ronald Chonillo <ronald.chonillo@beebtech.net>
2024-06-21 23:33:52 -0500

    [fix] test-new-mode-21

--- erpnext_ec/utilities/tools.py
+++ erpnext_ec/utilities/tools.py
@@ -5,17 +5,13 @@ import socket
 def get_full_url():
     # Obtener el nombre del host desde la solicitud actual
     host_name = frappe.utils.get_host_name_from_request()
+    port = ''
 
     # Verificar si está en modo DNS multitenant
-    if frappe.local.conf.get('dns_multitenant'):
-        # Usar el puerto configurado en caso de multitenancy
-        port = frappe.conf.get('webserver_port', 8000)  # Puerto por defecto 8000
-    else:
-        # Obtener el puerto configurado o usar un puerto por defecto
-        port = frappe.conf.http_port if hasattr(frappe.conf, 'http_port') else 80
+    if not frappe.local.conf.get('dns_multitenant'):    
+        port = ':'+frappe.local.conf.nginx_port
 
     # Construir la URL completa con el protocolo y el puerto
-    protocol = "https://" if frappe.local.request.scheme == "https" else "http://"
-    full_url = f"{protocol}{host_name}:{port}"
+    full_url = f"{host_name}{port}"
 
     return full_url
