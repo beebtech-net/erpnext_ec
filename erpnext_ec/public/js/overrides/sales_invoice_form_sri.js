96a2374545d18c2622ebcf649b04fd672dd26047
Ronald Chonillo <ronaldchonillobt@beebtech.net>
2024-07-24 22:37:00 -0500

    [add/fix] default values - 003

--- erpnext_ec/public/js/overrides/sales_invoice_form_sri.js
+++ erpnext_ec/public/js/overrides/sales_invoice_form_sri.js
@@ -6,13 +6,13 @@ frappe.ui.form.on(doctype_customized, {
         if (frm.doc.status == 'Draft')
         {
             //Fields for custom settings
-            frm.set_query('ptoemi', function() {
-                return {
-                    filters: {
-                        'sri_establishment_lnk': frm.doc.estab
-                    }
-                };
-            });
+            //frm.set_query('ptoemi', function() {
+            //    return {
+            //        filters: {
+            //            'sri_establishment_lnk': frm.doc.estab
+            //        }
+            //    };
+            //});
         }
 
         if (frm.doc.status == 'Cancelled' || frm.doc.status == 'Draft')
@@ -24,9 +24,9 @@ frappe.ui.form.on(doctype_customized, {
         //console.log(frm);
         //console.log(frm.doctype_customized);
     },
-    estab: function(frm)
-	{
+    //estab: function(frm)
+	//{
         //frm.set_value('ptoemi',  '');
         //frm.refresh_field('ptoemi');
-	},
+	//},
 })
