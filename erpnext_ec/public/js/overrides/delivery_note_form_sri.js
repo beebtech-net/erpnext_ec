1edb96d8fd369ef457644d00aad7a9000975bf06
Ronald Chonillo <ronaldchonillobt@beebtech.net>
2025-09-18 21:40:30 -0500

    [fix] create for delivery trip

--- erpnext_ec/public/js/overrides/delivery_note_form_sri.js
+++ erpnext_ec/public/js/overrides/delivery_note_form_sri.js
@@ -3,13 +3,21 @@ var doctype_customized = "Delivery Note";
 frappe.ui.form.on(doctype_customized, {
 	refresh(frm)
     {
-        frm.add_custom_button('<i class="fa fa-truck"></i> ' +  __('Delivery Trip'), function() {
-            frappe.new_doc('Delivery Trip', {
-                delivery_stops: [{
-                    delivery_note: frm.doc.name
-                }]
-            });
-        }, __('Create'));
+        if (cur_frm.doc.docstatus == 1 && frappe.model.can_create("Delivery Trip")) 
+        {
+			frm.add_custom_button(
+				__("Delivery Trip"),
+				function () {
+					
+					frappe.model.open_mapped_doc({
+		                method: "erpnext.stock.doctype.delivery_note.delivery_note.make_delivery_trip",
+		                frm: cur_frm,
+		            });
+					
+				},
+				__("Create")
+			);
+		}
 
         if (frm.doc.status == 'Cancelled' || frm.doc.status == 'Draft') {
             return false;
